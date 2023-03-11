b6d2f9a3496a649598fe6143dbadcd415f32dde6
Alexandre <alexandrejeronimo2001@gmail.com>
2023-03-11 15:44:01 +0000

    updating packages, mantine and tailwind fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,16 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
 import "~/styles/globals.css";
+import { MantineProvider } from "@mantine/core";
+import { emCache } from "lib/emotionCache";
 
 const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { session, ...pageProps } }) => {
+  const cache = emCache();
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <MantineProvider emotionCache={cache} withGlobalStyles withNormalizeCSS theme={{ colorScheme: "light" }}>
+        <Component {...pageProps} />
+      </MantineProvider>
     </SessionProvider>
   );
 };
