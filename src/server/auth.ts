b0ba4bbbef21b41acc88d37f83b9fb6eec77d4f1
tgn <trwygon@outlook.pt>
2023-04-26 00:52:31 +0100

    annoying eslint errors

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/require-await */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { type GetServerSidePropsContext } from "next";
 import { getServerSession, type DefaultSession, type NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
