eb21ab469b90862f0222799512bfe7d9e2140402
Alex-Jer <alexandrejeronimo2001@gmail.com>
2023-05-07 18:04:53 +0100

    added mapbox

--- src/pages/properties/[propertyId].tsx
+++ src/pages/properties/[propertyId].tsx
@@ -6,6 +6,8 @@ import { useRouter } from "next/router";
 import { useProperty } from "~/hooks/useQueries";
 import { Property } from "~/types";
 import { CardsCarousel } from "~/components/CardsCarousel";
+import Map, { Marker } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 const Property: NextPage = () => {
   const router = useRouter();
@@ -52,8 +54,8 @@ const Property: NextPage = () => {
   const renderHeader = (property: Property) => {
     return (
       <>
-        <div className="flex justify-between">
-          <h1>{property.title}</h1>
+        <div className="mb-2 flex justify-between">
+          <h1 className="text-4xl">{property.title}</h1>
           {property.current_price_sale ? (
             <div className="text-4xl">{property.current_price_sale}€</div>
           ) : (
@@ -88,6 +90,52 @@ const Property: NextPage = () => {
     );
   };
 
+  const renderDescription = (property: Property) => {
+    return (
+      <div className="mt-4">
+        <h2 className="text-3xl">Description</h2>
+        <div>{property.description}</div>
+      </div>
+    );
+  };
+
+  const renderMap = () => {
+    return (
+      <>
+        <h2 className="mt-4 text-3xl">Location</h2>
+        <div className="h-3/6 w-auto">
+          <Map
+            mapboxAccessToken="pk.eyJ1IjoiYWxleGplciIsImEiOiJjbGhkbHlqZ2UwbnJ1M2ZudmtuMnZuZnJwIn0.H-iqt45XSgHUA3wU35eQ7Q"
+            initialViewState={{
+              longitude: -8.83075580229916,
+              latitude: 39.7329434886251,
+              zoom: 15,
+            }}
+            mapStyle="mapbox://styles/mapbox/streets-v11"
+          >
+            <Marker longitude={-8.83075580229916} latitude={39.7329434886251}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="red"
+                className="h-6 w-6"
+              >
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+                />
+              </svg>
+            </Marker>
+          </Map>
+        </div>
+      </>
+    );
+  };
+
   return (
     <>
       <Head>
@@ -97,11 +145,8 @@ const Property: NextPage = () => {
 
       {renderHeader(property)}
       {renderGallery()}
-
-      <div className="mt-4">
-        <h2 className="text-xl">Description</h2>
-        <div>{property.description}</div>
-      </div>
+      {renderDescription(property)}
+      {renderMap()}
     </>
   );
 };
