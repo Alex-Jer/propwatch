d41d1a61021bd7c536673da3ef37ea5f44be2a86
Alex-Jer <alexandrejeronimo2001@gmail.com>
2023-05-11 12:18:00 +0100

    moved svgs to independent files

---
+++ public/icons/index.ts
@@ -0,0 +1,7 @@
+export { default as Apartment } from "./apartment";
+export { default as House } from "./house";
+export { default as Office } from "./office";
+export { default as Shop } from "./shop";
+export { default as Warehouse } from "./warehouse";
+export { default as Garage } from "./garage";
+export { default as Default } from "./default";
