5d57ab3f05bea0bb1bbdb0e5a6f00673f9fd1f2f
Alex-Jer <alexandrejeronimo2001@gmail.com>
2023-06-22 22:00:01 +0100

    extracted main info into its own component

--- src/components/AddPropertyDrawer.tsx
+++ src/components/AddPropertyDrawer.tsx
@@ -1,19 +1,17 @@
 import { useState } from "react";
 import { useForm, Controller } from "react-hook-form";
-import { TextInput, Textarea, Select, MultiSelect, NumberInput } from "react-hook-form-mantine";
+import { Select, NumberInput } from "react-hook-form-mantine";
 import { useInputState } from "@mantine/hooks";
-import { Button, Drawer, Loader, Stepper, Group, createStyles, Divider } from "@mantine/core";
-import { IconBathFilled, IconCurrencyEuro } from "@tabler/icons-react";
+import { Button, Drawer, Stepper, Group, createStyles, Divider } from "@mantine/core";
+import { IconCurrencyEuro } from "@tabler/icons-react";
 import { FilePond, registerPlugin } from "react-filepond";
 import "filepond/dist/filepond.min.css";
 import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
 import FilePondPluginImagePreview from "filepond-plugin-image-preview";
 import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
-import { useSession } from "next-auth/react";
-import { type SelectOption } from "~/types";
-import { useAllCollections, useTags } from "~/hooks/useQueries";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { AddPropertyMainInfo } from "./AddPropertyMainInfo";
 
 interface AddPropertyDrawerProps {
   opened: boolean;
@@ -30,50 +28,18 @@ const listingType = [
   { value: "both", label: "Both" },
 ];
 
-const propertyType = [
-  { value: "house", label: "House" },
-  { value: "apartment", label: "Apartment" },
-  { value: "office", label: "Office" },
-  { value: "shop", label: "Shop" },
-  { value: "warehouse", label: "Warehouse" },
-  { value: "garage", label: "Garage" },
-  { value: "land", label: "Land" },
-  { value: "other", label: "Other" },
-];
-
-const typology = [
-  { value: "t0", label: "T0" },
-  { value: "t1", label: "T1" },
-  { value: "t2", label: "T2" },
-  { value: "t3", label: "T3" },
-  { value: "t4", label: "T4" },
-  { value: "t5", label: "T5" },
-  { value: "t6", label: "T6" },
-  { value: "t7", label: "T7" },
-  { value: "t8", label: "T8" },
-  { value: "t9", label: "T9" },
-  { value: "t10", label: "T10" },
-];
-
-const currentStatus = [
-  { value: "available", label: "Available" },
-  { value: "unavailable", label: "Unavailable" },
-  { value: "unknown", label: "Unknown" },
-];
-
 const schema = z.object({
   Title: z.string().nonempty({ message: "A title is required" }),
   Description: z.string(),
   "Property Type": z.string(),
   Typology: z.string(),
   "Current Status": z.string(),
-  "Gross Area": z.number().positive().optional(),
-  "Net Area": z.number().positive().optional(),
-  "Number of Bathrooms": z.number().positive().optional(),
+  "Gross Area": z.number().nonnegative().optional(),
+  "Net Area": z.number().nonnegative().optional(),
+  "Number of Bathrooms": z.number().nonnegative().optional(),
   "Listing Type": z.string(),
-  "Current Sale Price": z.number().positive().optional(),
-  "Current Rent Price": z.number().positive().optional(),
-  "Media Type": z.string(),
+  "Current Sale Price": z.number().nonnegative().optional(),
+  "Current Rent Price": z.number().nonnegative().optional(),
   Tags: z.array(z.string()),
   Collections: z.array(z.string()),
   Files: z.array(z.any()),
@@ -92,14 +58,13 @@ const defaultValues: FormSchemaType = {
   "Listing Type": "",
   "Current Sale Price": undefined,
   "Current Rent Price": undefined,
-  "Media Type": "",
   Tags: [],
   Collections: [],
   Files: [],
   Blueprints: [],
 };
 
-type FormSchemaType = z.infer<typeof schema>;
+export type FormSchemaType = z.infer<typeof schema>;
 
 export function AddPropertyDrawer({ opened, close }: AddPropertyDrawerProps) {
   const { classes } = useStyles();
@@ -113,31 +78,10 @@ export function AddPropertyDrawer({ opened, close }: AddPropertyDrawerProps) {
   const nextStep = () => setStepperActive((current) => (current < TOTAL_STEPS ? current + 1 : current));
   const prevStep = () => setStepperActive((current) => (current > 0 ? current - 1 : current));
 
-  const { data: session, status } = useSession();
   const { control, handleSubmit } = useForm<FormSchemaType>({
     resolver: zodResolver(schema),
     defaultValues,
   });
-
-  const { data: tagsData, isLoading: tagsIsLoading } = useTags({ session, status });
-  const { data: collectionsData, isLoading: collectionsIsLoading } = useAllCollections({ session, status });
-
-  let tags = [] as SelectOption[];
-  let collections = [] as SelectOption[];
-
-  if (tagsData) {
-    tags = tagsData.map((tag) => ({
-      value: tag.id.toString(),
-      label: tag.name,
-    }));
-  }
-
-  if (collectionsData) {
-    collections = collectionsData.data.map((collection) => ({
-      value: collection.id.toString(),
-      label: collection.name,
-    }));
-  }
 
   return (
     <>
@@ -178,120 +122,7 @@ export function AddPropertyDrawer({ opened, close }: AddPropertyDrawerProps) {
             >
               <Stepper active={stepperActive} onStepClick={setStepperActive} breakpoint="sm">
                 <Stepper.Step label="Main Info">
-                  <TextInput
-                    className="mb-3"
-                    name="Title"
-                    label="Title"
-                    placeholder="Title"
-                    control={control}
-                    withAsterisk
-                  />
-                  <Textarea
-                    className="mb-3"
-                    name="Description"
-                    label="Description"
-                    placeholder="Description"
-                    control={control}
-                    autosize
-                    minRows={2}
-                    maxRows={5}
-                  />
-                  <Group className="mb-3" position="apart" grow>
-                    <Select
-                      data={propertyType}
-                      name="Property Type"
-                      label="Property Type"
-                      placeholder="Property Type"
-                      control={control}
-                      searchable
-                      nothingFound="No options"
-                    />
-                    <Select
-                      data={typology}
-                      name="Typology"
-                      label="Typology"
-                      placeholder="Typology"
-                      control={control}
-                      searchable
-                      creatable
-                      getCreateLabel={(query) => `+ Create ${query} `}
-                      onCreate={(query) => {
-                        const newTypology = { value: query, label: query };
-                        typology.push(newTypology);
-                        typology.sort((a, b) => a.label.localeCompare(b.label, undefined, { numeric: true }));
-                        return newTypology;
-                      }}
-                    />
-                    <Select
-                      data={currentStatus}
-                      name="Current Status"
-                      placeholder="Current Status"
-                      label="Current Status"
-                      control={control}
-                    />
-                  </Group>
-
-                  <Group className="mb-3" position="apart" grow>
-                    <NumberInput
-                      name="Gross Area"
-                      label="Gross Area"
-                      placeholder="Gross Area"
-                      control={control}
-                      icon="m²"
-                      step={5}
-                      min={0}
-                      styles={{ icon: { fontSize: "16px" } }}
-                    />
-                    <NumberInput
-                      name="Net Area"
-                      label="Net Area"
-                      placeholder="Net Area"
-                      control={control}
-                      icon="m²"
-                      step={5}
-                      min={0}
-                      styles={{ icon: { fontSize: "16px" } }}
-                    />
-                    <NumberInput
-                      name="Number of Bathrooms"
-                      label="Number of Bathrooms"
-                      placeholder="Bathrooms"
-                      control={control}
-                      icon={<IconBathFilled size="1rem" />}
-                      min={0}
-                    />
-                  </Group>
-
-                  <Group className="mb-3" position="apart" grow>
-                    <MultiSelect
-                      data={tags}
-                      name="Tags"
-                      label="Tags"
-                      placeholder="Tags"
-                      control={control}
-                      icon={tagsIsLoading && <Loader size="1rem" />}
-                      searchable
-                      clearable
-                      creatable
-                      getCreateLabel={(query) => `+ Create ${query} `}
-                      onCreate={(query) => {
-                        const newTag = { value: query, label: query };
-                        tags.push(newTag);
-                        tags.sort((a, b) => a.label.localeCompare(b.label));
-                        return newTag;
-                      }}
-                    />
-                    <MultiSelect
-                      data={collections}
-                      name="Collections"
-                      label="Collections"
-                      placeholder="Collections"
-                      control={control}
-                      icon={collectionsIsLoading && <Loader size="1rem" />}
-                      searchable
-                      clearable
-                    />
-                  </Group>
+                  <AddPropertyMainInfo control={control} />
                 </Stepper.Step>
 
                 <Stepper.Step label="Address"></Stepper.Step>
---
+++ src/components/AddPropertyMainInfo.tsx
@@ -0,0 +1,176 @@
+import { Group, Loader } from "@mantine/core";
+import { IconBathFilled } from "@tabler/icons-react";
+import { useSession } from "next-auth/react";
+import { type Control } from "react-hook-form";
+import { TextInput, Textarea, Select, MultiSelect, NumberInput } from "react-hook-form-mantine";
+import { useAllCollections, useTags } from "~/hooks/useQueries";
+import { type SelectOption } from "~/types";
+import { type FormSchemaType } from "./AddPropertyDrawer";
+
+const propertyTypes = [
+  { value: "house", label: "House" },
+  { value: "apartment", label: "Apartment" },
+  { value: "office", label: "Office" },
+  { value: "shop", label: "Shop" },
+  { value: "warehouse", label: "Warehouse" },
+  { value: "garage", label: "Garage" },
+  { value: "land", label: "Land" },
+  { value: "other", label: "Other" },
+];
+
+const typologies = [
+  { value: "t0", label: "T0" },
+  { value: "t1", label: "T1" },
+  { value: "t2", label: "T2" },
+  { value: "t3", label: "T3" },
+  { value: "t4", label: "T4" },
+  { value: "t5", label: "T5" },
+  { value: "t6", label: "T6" },
+  { value: "t7", label: "T7" },
+  { value: "t8", label: "T8" },
+  { value: "t9", label: "T9" },
+  { value: "t10", label: "T10" },
+];
+
+const currentStatuses = [
+  { value: "available", label: "Available" },
+  { value: "unavailable", label: "Unavailable" },
+  { value: "unknown", label: "Unknown" },
+];
+
+export function AddPropertyMainInfo({ control }: { control: Control<FormSchemaType> }) {
+  const { data: session, status } = useSession();
+  const { data: tagsData, isLoading: tagsIsLoading } = useTags({ session, status });
+  const { data: collectionsData, isLoading: collectionsIsLoading } = useAllCollections({ session, status });
+
+  let tags = [] as SelectOption[];
+  let collections = [] as SelectOption[];
+
+  if (tagsData) {
+    tags = tagsData.map((tag) => ({
+      value: tag.id.toString(),
+      label: tag.name,
+    }));
+  }
+
+  if (collectionsData) {
+    collections = collectionsData.data.map((collection) => ({
+      value: collection.id.toString(),
+      label: collection.name,
+    }));
+  }
+
+  return (
+    <div>
+      <h1>Property Main Info</h1>
+
+      <TextInput className="mb-3" name="Title" label="Title" placeholder="Title" control={control} withAsterisk />
+      <Textarea
+        className="mb-3"
+        name="Description"
+        label="Description"
+        placeholder="Description"
+        control={control}
+        autosize
+        minRows={2}
+        maxRows={5}
+      />
+      <Group className="mb-3" position="apart" grow>
+        <Select
+          data={propertyTypes}
+          name="Property Type"
+          label="Property Type"
+          placeholder="Property Type"
+          control={control}
+          searchable
+          nothingFound="No options"
+        />
+        <Select
+          data={typologies}
+          name="Typology"
+          label="Typology"
+          placeholder="Typology"
+          control={control}
+          searchable
+          creatable
+          getCreateLabel={(query) => `+ Create ${query} `}
+          onCreate={(query) => {
+            const newTypology = { value: query, label: query };
+            typologies.push(newTypology);
+            typologies.sort((a, b) => a.label.localeCompare(b.label, undefined, { numeric: true }));
+            return newTypology;
+          }}
+        />
+        <Select
+          data={currentStatuses}
+          name="Current Status"
+          placeholder="Current Status"
+          label="Current Status"
+          control={control}
+        />
+      </Group>
+
+      <Group className="mb-3" position="apart" grow>
+        <NumberInput
+          name="Gross Area"
+          label="Gross Area"
+          placeholder="Gross Area"
+          control={control}
+          icon="m²"
+          step={5}
+          min={0}
+          styles={{ icon: { fontSize: "16px" } }}
+        />
+        <NumberInput
+          name="Net Area"
+          label="Net Area"
+          placeholder="Net Area"
+          control={control}
+          icon="m²"
+          step={5}
+          min={0}
+          styles={{ icon: { fontSize: "16px" } }}
+        />
+        <NumberInput
+          name="Number of Bathrooms"
+          label="Number of Bathrooms"
+          placeholder="Bathrooms"
+          control={control}
+          icon={<IconBathFilled size="1rem" />}
+          min={0}
+        />
+      </Group>
+
+      <Group className="mb-3" position="apart" grow>
+        <MultiSelect
+          data={tags}
+          name="Tags"
+          label="Tags"
+          placeholder="Tags"
+          control={control}
+          icon={tagsIsLoading && <Loader size="1rem" />}
+          searchable
+          clearable
+          creatable
+          getCreateLabel={(query) => `+ Create ${query} `}
+          onCreate={(query) => {
+            const newTag = { value: query, label: query };
+            tags.push(newTag);
+            tags.sort((a, b) => a.label.localeCompare(b.label));
+            return newTag;
+          }}
+        />
+        <MultiSelect
+          data={collections}
+          name="Collections"
+          label="Collections"
+          placeholder="Collections"
+          control={control}
+          icon={collectionsIsLoading && <Loader size="1rem" />}
+          searchable
+          clearable
+        />
+      </Group>
+    </div>
+  );
+}
