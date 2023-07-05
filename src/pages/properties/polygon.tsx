58d65efc84f4497686af089a3f3b77adba86b66a
tgn <trwygon@outlook.pt>
2023-07-05 02:38:32 +0100

    feat: draw map polygons

--- src/pages/properties/polygon.tsx
+++ src/pages/properties/polygon.tsx
@@ -1,4 +1,4 @@
-import { IconBuildingEstate, IconMapSearch } from "@tabler/icons-react";
+import { IconMapSearch } from "@tabler/icons-react";
 import { type NextPage } from "next";
 import Head from "next/head";
 import { DisplayProperties } from "~/components/DisplayProperties";
@@ -6,14 +6,43 @@ import type { DisplayPropertiesProps } from "~/types";
 import Map from "react-map-gl";
 import { env } from "~/env.mjs";
 import "mapbox-gl/dist/mapbox-gl.css";
+import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
+import { Grid } from "@mantine/core";
+import { useCallback, useState } from "react";
+import DrawControl from "~/components/map/DrawControl";
+import type { DrawPolygon, DrawCreateEvent, DrawUpdateEvent, DrawDeleteEvent } from "@mapbox/mapbox-gl-draw";
 
 const Properties: NextPage<DisplayPropertiesProps> = ({ search, setSearch }) => {
+  const [features, setFeatures] = useState<{ [key: string]: DrawPolygon }>({});
+
+  console.log(features);
+
+  const onUpdate = useCallback((e: DrawCreateEvent | DrawUpdateEvent) => {
+    setFeatures((currFeatures) => {
+      const newFeatures: { [key: string]: DrawPolygon } = { ...currFeatures };
+      for (const f of e.features) {
+        if (f.id) newFeatures[f.id] = f.geometry as DrawPolygon;
+      }
+      return newFeatures;
+    });
+  }, []);
+
+  const onDelete = useCallback((e: DrawDeleteEvent) => {
+    setFeatures((currFeatures) => {
+      const newFeatures: { [key: string]: DrawPolygon } = { ...currFeatures };
+      for (const f of e.features) {
+        if (f.id) delete newFeatures[f.id];
+      }
+      return newFeatures;
+    });
+  }, []);
+
   const renderMap = () => {
     const latitude = 39.4;
     const longitude = -8.2;
     return (
       <>
-        <div className="h-3/6 w-1/2">
+        <div className="h-3/6 w-full">
           <Map
             mapboxAccessToken={env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
             initialViewState={{
@@ -23,7 +52,20 @@ const Properties: NextPage<DisplayPropertiesProps> = ({ search, setSearch }) => 
             }}
             style={{ width: "100%", height: "400px", borderRadius: "12px" }}
             mapStyle="mapbox://styles/mapbox/streets-v11"
-          ></Map>
+          >
+            <DrawControl
+              position="top-left"
+              displayControlsDefault={false}
+              controls={{
+                polygon: true,
+                trash: true,
+              }}
+              defaultMode="draw_polygon"
+              onCreate={onUpdate}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          </Map>
         </div>
       </>
     );
@@ -42,15 +84,16 @@ const Properties: NextPage<DisplayPropertiesProps> = ({ search, setSearch }) => 
       </div>
 
       <div className="-mx-4 mb-4 border-b border-shark-700" />
+      <Grid>
+        <Grid.Col span={12}>{renderMap()}</Grid.Col>
+      </Grid>
 
-      {renderMap()}
-
-      <div className="mb-2 flex flex-row items-center">
+      {/* <div className="mb-2 mt-6 flex flex-row items-center">
         <IconBuildingEstate className="-mt-1 mr-2" strokeWidth={1.5} />
         <h1 className="pb-1 text-base font-semibold">Properties</h1>
-      </div>
+      </div> */}
 
-      <div className="-mx-4 mb-4 border-b border-shark-700" />
+      <div className="-mx-4 mb-4 mt-4 border-b border-shark-700" />
 
       <DisplayProperties search={search} setSearch={setSearch} />
     </>
