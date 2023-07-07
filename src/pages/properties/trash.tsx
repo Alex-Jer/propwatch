fa22a424541f8f823b5185d9d15b5a17fde413f2
tgn <trwygon@outlook.pt>
2023-07-07 15:58:16 +0100

    fix: forgot to remove debug stuff

--- src/pages/properties/trash.tsx
+++ src/pages/properties/trash.tsx
@@ -142,7 +142,7 @@ const TrashedProperties: NextPage = () => {
       </div>
 
       <div className="-mx-4 mb-4 border-b border-shark-700" />
-      {properties?.length && properties?.length > 4 ? (
+      {properties?.length && properties?.length > 0 ? (
         <>
           <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4">
             {renderProperties(properties)}
