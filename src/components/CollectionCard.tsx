61542098e78c97faad4540f01af359f5d13044aa
tgn <trwygon@outlook.pt>
2023-07-07 04:15:28 +0100

    feat: collection collage (we waste a little time trollge)

--- src/components/CollectionCard.tsx
+++ src/components/CollectionCard.tsx
@@ -13,19 +13,60 @@ const useStyles = createStyles((theme) => ({
 }));
 
 interface CollectionCardProps {
-  image: string;
+  covers: string[];
   title: string;
   description: string;
   tags: string[];
   date: string;
 }
 
-export function CollectionCard({ image, description, title, tags, date }: CollectionCardProps) {
+type ThumbnailCollageProps = {
+  covers: string[];
+};
+
+export function CollectionCard({ covers, description, title, tags, date }: CollectionCardProps) {
   const { classes } = useStyles();
+
+  const ThumbnailCollage = ({ covers }: ThumbnailCollageProps) => {
+    return (
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(2, 1fr)",
+          gridTemplateRows: "repeat(2, 1fr)",
+          gridGap: "1px",
+          width: "140px",
+          height: "140px",
+        }}
+      >
+        {covers.map((coverUrl, index) => (
+          <div
+            key={index}
+            style={{
+              overflow: "hidden",
+            }}
+          >
+            <Image
+              src={coverUrl}
+              width={140}
+              height={140}
+              style={{
+                objectFit: "cover",
+              }}
+              alt={"asd"}
+              className="flex-shrink-0"
+            />
+          </div>
+        ))}
+      </div>
+    );
+  };
+
   return (
     <Card radius={0} padding={0}>
       <Group noWrap spacing={0}>
-        <Image src={image} height={140} width={140} alt={title} className="flex-shrink-0" />
+        <Image src={"https://placehold.co/400x400"} height={140} width={140} alt={title} className="flex-shrink-0" />
+        <ThumbnailCollage covers={covers} />
 
         <div className={classes.body}>
           <Text className={classes.title} mb="xs">
